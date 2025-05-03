fdfa32e8d7cff5cc7256f16575bbf633ac466864
SBTabanar <sergeitabanar7@gmail.com>
2025-05-03 17:42:44 +0800

    Initial Commit

---
+++ BonsandBlooms/frmLogin.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BonsandBlooms
+{
+    public partial class frmLogin : Form
+    {
+        BonsandBlooms frm;
+        public frmLogin(BonsandBlooms frm)
+        {
+            InitializeComponent();
+            this.frm = frm;
+        }
+        DatabaseConnect config = new DatabaseConnect();
+        usableFunction func = new usableFunction();
+        string query;
+        int maxrow;
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            query = "SELECT * FROM tblUser WHERE U_UNAME ='" + UsernameTextBox.Text + "' AND U_PASS = '" + PasswordTextBox.Text + "'";
+            maxrow = config.maxrow(query);
+
+            if(maxrow > 0)
+            {
+                MessageBox.Show("User successfully logged in");
+                frm.enable_menu();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Account does not exist!","Invalid",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UsernameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
